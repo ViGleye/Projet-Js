6e8ef570ef85ade7a3961dbff9e7daef1d9ea8f8
Victorien <146822407+ViGleye@users.noreply.github.com>
2024-05-03 17:03:45 +0200

    first communit for switch dropdown in combobox

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import axios from "axios"
 import Link from "next/link"
 import { useState } from "react"
-import MyCombobox from "@/components/FilterCombobox.jsx"
-import Button from "@/components/Button.jsx"
+import Dropdown from "@/components/Dropdown"
+import Button from "@/components/Button"
 export const getServerSideProps = async () => {
   const response = await axios.get("http://localhost:3000/api/locations")
   const initialLocations = response.data
 
-  
-return {
+  return {
     props: { initialLocations },
   }
 }
@@ -34,55 +33,14 @@ export default function Home({ initialLocations }) {
   }
   const applyFilters = () => {
     const filtered = initialLocations.filter((location) => {
-      const isFreeMatch =
-        filters.isFree !== "" ? location.isFree === filters.isFree : true
-      const priceMatch =
-        !filters.isFree && filters.price
-          ? location.price === filters.price
-          : true
-      const pricesMatch =
-        !filters.isFree && filters.prices
-          ? location.prices === filters.prices
-          : true
-      const typeOfParcMatch = filters.typeOfParc
-        ? location.typeOfParc === filters.typeOfParc
-        : true
-      const isPrivateMatch = filters.isPrivate
-        ? location.isPrivate === filters.isPrivate
-        : true
-      const typesOfArtMatch = filters.typesOfArt
-        ? location.typesOfArt === filters.typesOfArt
-        : true
-      const ArtisticCurrentMatch = filters.ArtisticCurrent
-        ? location.ArtisticCurrent === filters.ArtisticCurrent
-        : true
-      const restaurantTypeMatch = filters.restaurantType
-        ? location.restaurantType === filters.restaurantType
-        : true
-      const starsMatch = filters.stars
-        ? location.stars === filters.stars
-        : true
-      const typeOfBarMatch = filters.typeOfBar
-        ? location.typeOfBar === filters.typeOfBar
-        : true
-      const typeMatch = filters.type ? location.type === filters.type : true
-
-      
-return (
-        typeMatch &&
-        typeOfParcMatch &&
-        typeOfBarMatch &&
-        ArtisticCurrentMatch &&
-        restaurantTypeMatch &&
-        typesOfArtMatch &&
-        isPrivateMatch &&
-        starsMatch &&
-        isFreeMatch &&
-        priceMatch &&
-        pricesMatch
-      )
+      return Object.keys(filters).every((key) => {
+        const filterValue = filters[key]
+        if (filterValue === "") return true
+        if (key === "isFree")
+          return location[key] === (filterValue === "Gratuit")
+        return location[key] === filterValue
+      })
     })
-
     setLocations(filtered)
   }
   const resetFilters = () => {
@@ -104,197 +62,229 @@ return (
 
   return (
     <main>
-      <h1 className="font-weight: 800 text-xl text-yellow-500">Filtre</h1>
-      <MyCombobox
-        options={Array.from(new Set(initialLocations.map(({ type }) => type)))}
-        selectedOption={filters.type}
-        setSelectedOption={(value) => handleFilterChange("type", value)}
+      <h1 className="text-xl font-bold text-yellow-500">Filtre</h1>
+      <Dropdown
+        title="Type"
+        options={Array.from(
+          new Set(initialLocations.map((location) => location.type)),
+        )}
+        selected={filters.type}
+        onChange={(value) => handleFilterChange("type", value)}
       />
-
-      {filters.type === "Parc" && (
+      {filters.type === "Musée" && (
         <>
-          <MyCombobox
-            options={initialLocations
-              .filter(({ type }) => type === "Parc")
-              .map(({ typeOfParc }) => typeOfParc)}
-            selectedOption={filters.typeOfParc}
-            setSelectedOption={(value) =>
-              handleFilterChange("typeOfParc", value)
-            }
+          <Dropdown
+            title="Types d'Art"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Musée")
+                  .map((location) => location.typesOfArt),
+              ),
+            )}
+            selected={filters.typesOfArt}
+            onChange={(value) => handleFilterChange("typesOfArt", value)}
           />
-          <MyCombobox
+          <Dropdown
+            title="Courant Artistique"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Musée")
+                  .map((location) => location.ArtisticCurrent),
+              ),
+            )}
+            selected={filters.ArtisticCurrent}
+            onChange={(value) => handleFilterChange("ArtisticCurrent", value)}
+          />
+          <Dropdown
+            title="Accès"
             options={["Gratuit", "Payant"]}
-            selectedOption={filters.isFree === true ? "Gratuit" : "Payant"}
-            setSelectedOption={(value) =>
+            selected={filters.isFree === true ? "Gratuit" : "Payant"}
+            onChange={(value) =>
               handleFilterChange("isFree", value === "Gratuit")
             }
           />
-
-          {!filters.isFree && (
-            <div>
-              <MyCombobox
-                options={initialLocations
-                  .filter(({ type }) => type === "Parc" && !location.isFree)
-                  .map(({ price }) => price)}
-                selectedOption={filters.price}
-                setSelectedOption={(value) =>
-                  handleFilterChange("price", value)
-                }
-              />
-              <MyCombobox
-                options={initialLocations
-                  .filter(({ type }) => type === "Parc" && !location.isFree)
-                  .map(({ prices }) => prices)}
-                selectedOption={filters.prices}
-                setSelectedOption={(value) =>
-                  handleFilterChange("prices", value)
-                }
-              />
-            </div>
-          )}
         </>
       )}
+      {filters.type === "Musée" && !filters.isFree ? (
+        <div>
+          <Dropdown
+            title="Prix moyen"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Musée")
+                  .map((location) => location.price),
+              ),
+            )}
+            selected={filters.price}
+            onChange={(value) => handleFilterChange("price", value)}
+          />
+          <Dropdown
+            title="Prix"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Musée")
+                  .map((location) => location.prices),
+              ),
+            )}
+            selected={filters.prices}
+            onChange={(value) => handleFilterChange("prices", value)}
+          />
+        </div>
+      ) : null}
 
       {filters.type === "Restaurant" && (
         <>
-          <MyCombobox
-            options={initialLocations
-              .filter(({ type }) => type === "Restaurant")
-              .map(({ restaurantType }) => restaurantType)}
-            selectedOption={filters.restaurantType}
-            setSelectedOption={(value) =>
-              handleFilterChange("restaurantType", value)
-            }
+          <Dropdown
+            title="Type de Restaurant"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Restaurant")
+                  .map((location) => location.restaurantType),
+              ),
+            )}
+            selected={filters.restaurantType}
+            onChange={(value) => handleFilterChange("restaurantType", value)}
           />
-          {
-            <MyCombobox
-              options={initialLocations
-                .filter(({ type }) => type === "Restaurant")
-                .map(({ stars }) => stars)}
-              selectedOption={filters.stars}
-              setSelectedOption={(value) => handleFilterChange("stars", value)}
-            />
-          }
-          <MyCombobox
+          <Dropdown
+            title="Etoiles"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Restaurant")
+                  .map((location) => location.stars),
+              ),
+            )}
+            selected={filters.stars}
+            onChange={(value) => handleFilterChange("stars", value)}
+          />
+          <Dropdown
+            title="Prix moyen"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Restaurant")
+                  .map((location) => location.price),
+              ),
+            )}
+            selected={filters.prices}
+            onChange={(value) => handleFilterChange("price", value)}
+          />
+        </>
+      )}
+      {filters.type === "Parc" && (
+        <>
+          <Dropdown
+            title="Type de Parc"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Parc")
+                  .map((location) => location.typeOfParc),
+              ),
+            )}
+            selected={filters.typeOfParc}
+            onChange={(value) => handleFilterChange("typeOfParc", value)}
+          />
+          <Dropdown
+            title="Privé ou Public"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Parc")
+                  .map((location) => location.isPrivate),
+              ),
+            )}
+            selected={filters.isPrivate ? "Privé" : "Public"}
+            onChange={(value) => handleFilterChange("isPrivate", value)}
+          />
+          <Dropdown
+            title="Accès"
             options={["Gratuit", "Payant"]}
-            selectedOption={filters.isFree === true ? "Gratuit" : "Payant"}
-            setSelectedOption={(value) =>
+            selected={filters.isFree === true ? "Gratuit" : "Payant"}
+            onChange={(value) =>
               handleFilterChange("isFree", value === "Gratuit")
             }
           />
-
-          {!filters.isFree && (
-            <MyCombobox
-              options={initialLocations
-                .filter(({ type }) => type === "Restaurant" && !location.isFree)
-                .map(({ price }) => price)}
-              selectedOption={filters.price}
-              setSelectedOption={(value) => handleFilterChange("price", value)}
-            />
-          )}
         </>
       )}
-
+      {filters.type === "Parc" && !filters.isFree ? (
+        <div>
+          <Dropdown
+            title="Prix moyen"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Parc")
+                  .map((location) => location.price),
+              ),
+            )}
+            selected={filters.price}
+            onChange={(value) => handleFilterChange("price", value)}
+          />
+          <Dropdown
+            title="Prix"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Parc")
+                  .map((location) => location.prices),
+              ),
+            )}
+            selected={filters.prices}
+            onChange={(value) => handleFilterChange("prices", value)}
+          />
+        </div>
+      ) : null}
       {filters.type === "Bar" && (
         <>
-          <MyCombobox
-            options={initialLocations
-              .filter(({ type }) => type === "Bar")
-              .map(({ typeOfBar }) => typeOfBar)}
-            selectedOption={filters.typeOfBar}
-            setSelectedOption={(value) =>
-              handleFilterChange("typeOfBar", value)
-            }
+          <Dropdown
+            title="Type de Bar"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Bar")
+                  .map((location) => location.typeOfBar),
+              ),
+            )}
+            selected={filters.typeOfBar}
+            onChange={(value) => handleFilterChange("typeOfBar", value)}
           />
-          <MyCombobox
-            options={["Gratuit", "Payant"]}
-            selectedOption={filters.isFree === true ? "Gratuit" : "Payant"}
-            setSelectedOption={(value) =>
-              handleFilterChange("isFree", value === "Gratuit")
-            }
+          <Dropdown
+            title="Prix Moyen"
+            options={Array.from(
+              new Set(
+                initialLocations
+                  .filter((location) => location.type === "Bar")
+                  .map((location) => location.price),
+              ),
+            )}
+            selected={filters.price}
+            onChange={(value) => handleFilterChange("price", value)}
           />
-
-          {!filters.isFree && (
-            <MyCombobox
-              options={initialLocations
-                .filter(({ type }) => type === "Bar" && !location.isFree)
-                .map(({ price }) => price)}
-              selectedOption={filters.price}
-              setSelectedOption={(value) => handleFilterChange("price", value)}
-            />
-          )}
         </>
       )}
-      {filters.type === "Musée" && (
-        <>
-          <MyCombobox
-            options={initialLocations
-              .filter(({ type }) => type === "Musée")
-              .map(({ typesOfArt }) => typesOfArt)}
-            selectedOption={filters.typesOfArt}
-            setSelectedOption={(value) =>
-              handleFilterChange("typesOfArt", value)
-            }
-          />
-          <MyCombobox
-            options={initialLocations
-              .filter(({ type }) => type === "Musée")
-              .map(({ ArtisticCurrent }) => ArtisticCurrent)}
-            selectedOption={filters.ArtisticCurrent}
-            setSelectedOption={(value) =>
-              handleFilterChange("ArtisticCurrent", value)
-            }
-          />
-          <MyCombobox
-            options={["Gratuit", "Payant"]}
-            selectedOption={filters.isFree === true ? "Gratuit" : "Payant"}
-            setSelectedOption={(value) =>
-              handleFilterChange("isFree", value === "Gratuit")
-            }
-          />
+      {/* Vous pouvez ajouter d'autres Dropdown pour d'autres types avec une logique similaire */}
+      <Button onClick={resetFilters} text="Reset Filter" />
 
-          {!filters.isFree && (
-            <div>
-              <MyCombobox
-                options={initialLocations
-                  .filter(({ type }) => type === "Musée" && !location.isFree)
-                  .map(({ price }) => price)}
-                selectedOption={filters.price}
-                setSelectedOption={(value) =>
-                  handleFilterChange("price", value)
-                }
-              />
-              <MyCombobox
-                options={initialLocations
-                  .filter(({ type }) => type === "Musée" && !location.isFree)
-                  .map(({ prices }) => prices)}
-                selectedOption={filters.prices}
-                setSelectedOption={(value) =>
-                  handleFilterChange("prices", value)
-                }
-              />
-            </div>
-          )}
-        </>
-      )}
-
-      <Button onClick={resetFilters} text="ResetFilter"></Button>
-
-      <h1 className="font-weight: 800 text-xl text-yellow-500">List</h1>
-
+      <h1 className="text-xl font-bold text-yellow-500">Liste</h1>
       <ul className="flex flex-col gap-4">
         {locations.map(
           ({ _id, name, type, street, city, zipCode, country }) => (
             <li key={_id} className="group flex items-center gap-2">
               <Link
                 href={`/locations/${_id}`}
-                className="flex gap-2 py-1 underline decoration-solid  rounded bg-blue-400 text-white px-4 "
+                className="flex gap-2 py-1 underline decoration-solid rounded bg-blue-400 text-white px-4"
               >
-                Nom de l'enseigne : {name}, Type: {type}, Adresse: {street},{" "}
+                Nom de l'enseigne: {name}, Type: {type}, Adresse: {street},{" "}
                 {city}, {zipCode}, {country}
               </Link>
             </li>
-          )
+          ),
         )}
       </ul>
     </main>
